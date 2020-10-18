25ec59194e254f5e24d87b2c6e84a459ec1d4969
Alexander Kachkaev <alexander@kachkaev.ru>
2020-10-18 14:35:01 +0100

    Copy linting rules from Concentra’s internal packages (action approved by CTO)

--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,6 +1,10 @@
 module.exports = {
   extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:json/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
@@ -9,47 +13,135 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "import", "json", "simple-import-sort"],
+  reportUnusedDisableDirectives: true,
   rules: {
     "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+    "@typescript-eslint/ban-ts-comment": [
+      "error",
+      {
+        "ts-expect-error": "allow-with-description",
+        "ts-ignore": "allow-with-description",
+        minimumDescriptionLength: 10,
+      },
+    ],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": [
+      "error",
+      { accessibility: "no-public" },
+    ],
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
+      {
+        selector: "class",
+        format: ["StrictPascalCase"],
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+      {
+        selector: ["variableLike"],
+        format: ["strictCamelCase", "StrictPascalCase"],
+        filter: {
+          regex: "^_$",
+          match: false,
+        },
+      },
+      {
+        selector: ["memberLike"],
+        leadingUnderscore: "allow",
+        filter: {
+          regex: "^(__ANT_.*|_)$",
+          match: false,
+        },
+        format: ["strictCamelCase", "StrictPascalCase"],
+      },
+    ],
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-parameter-properties": "error",
+    "@typescript-eslint/no-shadow": ["error"],
+    "@typescript-eslint/no-unused-expressions": "error",
     "@typescript-eslint/no-unused-vars": [
       "error",
       { ignoreRestSiblings: true },
     ],
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-unused-expressions": [
-      "error",
-      { allowTaggedTemplates: true },
-    ],
-
-    "import/default": "off",
-    "import/export": "off",
-    "import/order": "off",
-    "import/no-duplicates": "error",
-    "import/no-named-as-default": "off",
-    "import/no-named-as-default-member": "error",
-    "import/no-unresolved": "error",
+    // "@typescript-eslint/restrict-plus-operands": "error", // https://github.com/typescript-eslint/typescript-eslint/issues/386
+    curly: "error",
+    eqeqeq: "error",
+    "func-style": "error",
+    "import/default": "off", // Covered by TSC
+    "import/export": "off", // Covered by TSC
+    "import/first": "error",
     "import/named": "off", // https://github.com/benmosher/eslint-plugin-import/issues/1341
-    "import/namespace": "error",
-
+    "import/namespace": "off", // Covered by TSC + drops performance significantly
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error", // warning by default
+    "import/no-named-as-default-member": "off", // Too opinionated
+    "import/no-named-as-default": "off", // Too opinionated
+    "import/no-unresolved": ["error", { ignore: [".svg$"] }],
+    "import/no-useless-path-segments": ["error", { noUselessIndex: true }],
+    "import/order": "off", // See simple-import-sort/sort
+    // "import/order": ["error", { "newlines-between": "never" }], // Also see https://github.com/benmosher/eslint-plugin-import/pull/1105
+    "jest/no-test-callback": "off", // not compatible with testing Task.fork() etc.
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+    "no-alert": "error",
     "no-console": "error",
-    "no-shadow": "error",
-    "no-unused-vars": "off",
-    "object-shorthand": ["error", "always"],
+    "no-eval": "error",
+    "no-restricted-imports": [
+      "error",
+      {
+        // Generate list of not allowed imports: ".", "..", "../..", "../../..", "../../../..", and so on
+        paths: Array.from({ length: 10 }).map((_, depth) => ({
+          name: "../".repeat(depth).slice(0, -1) || ".",
+          message:
+            "Please don not import from parent index files as this may cause crashes due to cyclic dependencies. Use something like ../../path/to/stuff instead.",
+        })),
+      },
+    ],
+    "newline-before-return": "error",
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "TSEnumDeclaration",
+        message:
+          'Use TypeScript unions instead of enums (e.g type Foo = "a" | "b" | "c")', // https://github.com/typescript-eslint/typescript-eslint/issues/561
+      },
+    ],
+    "no-underscore-dangle": "error",
+    "object-shorthand": "error",
+    "prefer-arrow-callback": "error",
+    "prefer-template": "error",
     "simple-import-sort/sort": "error",
-    "sort-imports": "off",
+    "sort-imports": "off", // See simple-import-sort/sort
+    "spaced-comment": ["error", "always", { block: { balanced: true } }],
   },
   overrides: [
     {
+      // Avoid "'module'|'console' is not defined" (caused by no-undef)
       files: ["*.js"],
+      env: { node: true },
+    },
+    {
+      // Allow tool configurations to require("module-name")
+      files: ["*.config.js"],
       rules: {
         "@typescript-eslint/no-var-requires": "off",
       },
     },
     {
-      files: ["*.test.ts", "*.test.tsx"],
+      // Avoid replacing "///" with "// /" in the autogenerated next-env.d.ts without having to maintain this path in .eslintignore
+      files: ["next-env.d.ts"],
       rules: {
-        "@typescript-eslint/no-non-null-assertion": "off",
+        "spaced-comment": "off",
       },
     },
   ],
--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,12 +1,28 @@
 module.exports = {
-  extends: ["@kachkaev/eslint-config-base", "plugin:react/recommended"],
+  extends: [
+    "@kachkaev/eslint-config-base",
+    "plugin:react/recommended",
+    "plugin:testing-library/react",
+  ],
   plugins: ["react", "react-hooks"],
   rules: {
-    "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react/jsx-boolean-value": ["error", "always"],
+    "react/jsx-curly-brace-presence": "error",
+    "react/jsx-fragments": "error",
+    "react/no-danger": "error",
+    "react/no-render-return-value": "off", // Drops performance, no real value
     "react/prop-types": "off",
+    "react/self-closing-comp": "error",
   },
-  settings: {
-    react: { version: "detect" },
-  },
+  settings: { react: { version: "detect" } },
+  overrides: [
+    {
+      files: ["*.test.ts", "*.test.tsx"],
+      rules: {
+        "react/display-name": "off",
+      },
+    },
+  ],
 };
