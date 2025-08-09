3ee9dabbd35cd4060fe57e32fbfaadfb502fced1
Alexander Kachkaev <alexander@kachkaev.ru>
2025-08-09 20:17:40 +0100

    Update prettier plugins, fix plugin imports in pnpm repos

--- packages/prettier-config/main.js
+++ packages/prettier-config/main.js
@@ -1,6 +1,9 @@
+import * as prettierPluginPackageJson from "prettier-plugin-packagejson";
+import * as prettierPluginSh from "prettier-plugin-sh";
+
 /** @type {import("prettier").Config} */
 const config = {
-  plugins: ["prettier-plugin-packagejson", "prettier-plugin-sh"],
+  plugins: [prettierPluginPackageJson, prettierPluginSh],
 };
 
 // eslint-disable-next-line import/no-default-export -- required for Prettier
