b91f8f7d96192f999d9e748fae7054ea20ecfbca
Alexander Kachkaev <alexander@kachkaev.ru>
2025-11-02 14:55:47 +0000

    Switch to pnpm (#40)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,4 +3,18 @@ module.exports = {
     "@kachkaev/eslint-config-react",
     "@kachkaev/eslint-config-react/extra-type-checking",
   ],
+
+  rules: {
+    "@typescript-eslint/no-parameter-properties": "off",
+  },
+
+  overrides: [
+    {
+      files: ["*.cjs"],
+      rules: {
+        "@typescript-eslint/no-require-imports": "off",
+        "unicorn/no-empty-file": "off",
+      },
+    },
+  ],
 };
